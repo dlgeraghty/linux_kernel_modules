236a922a3b75912bdcd897c5acba7cbf8566219e
dlgeraghty <dvd.loge@gmail.com>
2020-05-29 20:51:56 +0200

    hello world & makefile

---
+++ hello_world.c
@@ -0,0 +1,19 @@
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+
+int init_module(void){
+
+	printk(KERN_INFO "Hello world this is my first module.\n");
+
+	return 0;
+
+}
+
+void cleanup_module(void){
+
+	printk(KERN_INFO "our first module ran smoothly!\n");
+
+}
