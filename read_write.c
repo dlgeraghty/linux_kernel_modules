9e46c83d9748ff37f05a7928e85e328314946175
dlgeraghty <dvd.loge@gmail.com>
2020-06-25 23:12:54 +0200

    write function and convert everything from file_operations to proc_ops

--- read_write.c
+++ read_write.c
@@ -2,31 +2,45 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
-#include <linux/string.h>
 #include <linux/vmalloc.h>
-#include <linux/uaccess.h>
-#include <linux/slab.h>
+#include <linux/fs.h>
 
 MODULE_LICENSE("GPL");
 
 static struct proc_dir_entry *proc_entry;
+
 
 static ssize_t my_custom_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
 
 }
 
 static ssize_t my_custom_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
+	char * str;
+	str = vmalloc((size_t) len);
+	if(copy_from_user(str, buf, len)){
+		vfree(str);
+		return -EFAULT;
+	}
+
+	str[len] = '\0';
+
+	printk(KERN_DEBUG "You entered: %s\n", str);
+
+	vfree(str);
+	*off += len;
+	return len;
 
 }
 
-static const struct file_operations proc_entry_fops = {
-	.read = my_custom_read;
-	.write = my_custom_write;
+static const struct proc_ops proc_entry_fops = {
+    .proc_read = my_custom_read,
+    .proc_write = my_custom_write,
 };
 
 int my_custom_init(void){
 	int ret = 0;
-	proc_entry = proc_create("rw", 0666, NULL, &proc_entry_fops);
+	proc_entry = proc_create( "modlist", 0666, NULL, &proc_entry_fops);
+
 	if(proc_entry == NULL){
 		ret = -ENOMEM;
 		printk(KERN_DEBUG "Cant create that proc entry\n");
@@ -34,6 +48,11 @@ int my_custom_init(void){
 		printk(KERN_DEBUG "Module correctly loaded...");
 	}
 	return ret;
+}
+
+void my_custom_exit(void){
+
+
 }
 
 module_init( my_custom_init );
